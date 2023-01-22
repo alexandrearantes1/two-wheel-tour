c63068104ad6ee1985176a76c81f443c7ec810b5
Alexandre Arantes <chan.arantes@gmail.com>
2023-01-22 20:08:32 +0000

    Remove 'dealers and garages' page, add tours, rent and contact pages, changed color pallete

--- assets/js/menu-toggler.js
+++ assets/js/menu-toggler.js
@@ -3,5 +3,7 @@ function toggleNav () {
    const hamb = document.querySelectorAll('#hamb')[0];
    
    menu.classList.toggle('show-menu');
+   menu.classList.toggle('glass');
+   menu.classList.toggle('shadow');
    hamb.classList.toggle('close-hamb');
 }
